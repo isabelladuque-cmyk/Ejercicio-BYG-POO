dd40af78fde1fe123937b1a5807ec7c7aa25d440
isabelladuque-cmyk <isabella.duque@autonoma.edu.co>
2026-06-29 18:25:12 -0500

    Descuento con sus clases hijas

--- src/main/java/org/isa/discount/system/FixedDiscount.java
+++ src/main/java/org/isa/discount/system/FixedDiscount.java
@@ -4,11 +4,19 @@ public class FixedDiscount extends Discount {
     private double discountDeducted;
 
     public FixedDiscount(double fixedDiscount) {
+        if (fixedDiscount < 0) {
+            throw new RuntimeException("El descuento no puede ser negativo");
+        }
         this.discountDeducted = fixedDiscount;
     }
 
     @Override
     public double applyDiscount(double totalAmount) {
-        return "Cupón de descuento fijo por: " + discountDeducted);
+        return Math.min(discountDeducted, totalAmount);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Cupón de descuento fijo por: $" + discountDeducted;
     }
 }
--- src/main/java/org/isa/discount/system/PercentageDiscount.java
+++ src/main/java/org/isa/discount/system/PercentageDiscount.java
@@ -1,4 +1,22 @@
 package org.isa.discount.system;
 
-public class PercentageDiscount {
+public class PercentageDiscount extends Discount {
+    private double percentage;
+
+    public PercentageDiscount(double percentage) {
+        if (percentage < 0 || percentage > 100) {
+            throw new RuntimeException("El porcentaje de descuento debe estar entre 0 y 100.");
+        }
+        this.percentage = percentage;
+    }
+
+    @Override
+    public double applyDiscount(double totalAmount) {
+        return totalAmount * (percentage / 100);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Porcentaje del descuento " + this.percentage;
+    }
 }
