2113db3cb2e6bf4c3c6693b768668029b2773280
isabelladuque-cmyk <isabella.duque@autonoma.edu.co>
2026-06-29 17:29:16 -0500

    creditCard terminada

--- src/main/java/org/isa/payments/CreditCard.java
+++ src/main/java/org/isa/payments/CreditCard.java
@@ -7,12 +7,13 @@ public class CreditCard extends PaymentMethod {
     public CreditCard(String number, String cvv) {
     this.number = number;
     this.cvv = cvv;
-    
+
+
+    }
 
     @Override
     public void processPayment(double amount) {
-        System.out.println("Cobrando " + amount + " a la tarjeta de crédito con el código " + cvv);
-
-
+        System.out.println("Cobrando $" + amount + " a la tarjeta de crédito terminada en: "
+                number + "con el CVV " + cvv );
     }
 }
